56269045d980292103050a59c13c17e3974f6b22
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-05 18:28:23 -0500

    Update EC2 variables with descriptions and defaults

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -1,16 +1,23 @@
 variable "instance_count" {
-    default = 1
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 3
 }
 
 variable "instance_type" {
-    default = "c7i-flex.large"
+  description = "EC2 instance type"
+  type        = string
+  default     = "c7i-flex.large"
 }
 
 variable "instance_name" {
-    default = "Jenkins-Training"
+  description = "Base name for EC2 instances"
+  type        = string
+  default     = "Jenkins-Training"
 }
 
 variable "public_key" {
   description = "SSH public key for EC2 access"
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkc8go/R/OI8v5S7pyR625dilCCGDxbVHrp8ioA/swk divya@Divya"
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkc8go/R/OI8v5S7pyR625dilCCGDxbVHrp8ioA/swk divya@Divya"
 }
