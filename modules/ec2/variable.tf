ed9e9e63d1fb875a920c09d29bbcb83e8c8446e7
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-05 18:27:03 -0500

    Add public_key variable for EC2 access

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -9,3 +9,8 @@ variable "instance_type" {
 variable "instance_name" {
     default = "Jenkins-Training"
 }
+
+variable "public_key" {
+  description = "SSH public key for EC2 access"
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkc8go/R/OI8v5S7pyR625dilCCGDxbVHrp8ioA/swk divya@Divya"
+}
