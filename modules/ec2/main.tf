e732fe4caa48388c319fb4f9de874633f68dc731
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-05 18:29:24 -0500

    Add EC2 key pair resource and associate with instance

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,12 +1,20 @@
 provider "aws" {
 region = "us-east-1"
 }
- 
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "mynewkey"
+  public_key = var.public_key
+}
+
  resource "aws_instance" "ec2_instance" {
-  count = var.instance_count
+  count         = var.instance_count
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = var.instance_type
+
+  key_name = aws_key_pair.my_key.key_name
+
   tags = {
     Name = "${var.instance_name}-${count.index + 1}"
-  } 
+  }
 }
