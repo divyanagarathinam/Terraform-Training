3b5dd16fc7988d42aa6a1bf609b8ca0fa5bafe0e
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-22 17:22:02 -0400

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,7 @@
 output "ec2_instance_ip" {
-    value = aws_instance.ec2_instance[0].public_ip
+   /* value = aws_instance.ec2_instance[0].public_ip*/
+  value = {
+    for name, inst in aws_instance.ec2_instance :
+    name => inst.public_ip
+  }
 }
